1bf994e58c4d7a13817906d9ca282ead5762ed78
Philip Fuster <pafus23@protonmail.com>
2024-09-27 02:07:21 -0400

    initialize project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+// biome-ignore lint/style/noNonNullAssertion: defined in index.html
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
