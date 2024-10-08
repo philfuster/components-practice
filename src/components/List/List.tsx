44851d204b4ceebabf61a432bb078795ec2a6a3b
Philip Fuster <pafus23@protonmail.com>
2024-10-08 00:29:06 -0400

    collocate types in component file in a region block up top for List component.

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,5 +1,31 @@
-import type { ItemBase, ListProps } from "./types/list";
+import type { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
+import type { Remap } from "../../shared/utilityTypes";
 
+// #region local type definitions
+export interface ItemBase {
+	id: string;
+	content: string;
+}
+
+type NativeContainerProps = Remap<
+	DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+>;
+
+interface ListContainerProps extends Omit<NativeContainerProps, "children"> {}
+
+export interface ListItemInjectedProps<TItem extends ItemBase> {
+	item: TItem;
+	index: number;
+	ListItem: (props: ItemBase) => ReactNode;
+}
+
+export interface ListProps<TItem extends ItemBase> extends ListContainerProps {
+	items: TItem[];
+	children: (props: ListItemInjectedProps<TItem>) => ReactNode;
+	header?: ReactNode;
+	footer?: ReactNode;
+}
+// #endregion
 const ListItem = (props: ItemBase) => (
 	<li className="list-item">{props.content}</li>
 );
